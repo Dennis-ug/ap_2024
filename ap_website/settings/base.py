4d91dea6b8d5d41997347a6515e95651cd2cd881
Dennis-ug <dennismawanda2@gmail.com>
2024-08-01 11:21:03 +0300

    v1

--- ap_website/settings/base.py
+++ ap_website/settings/base.py
@@ -140,8 +140,8 @@ STATICFILES_DIRS = [
 # MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 # MEDIA_URL = "/media/"
 
-STATIC_URL = "static/"
-MEDIA_URL = "media/"
+STATIC_URL = "/static/"
+MEDIA_URL = "/media/"
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
