52f0e5b7aad096dea7ff2da46e6819a2d25b600f
Dennis-ug <dennismawanda2@gmail.com>
2024-08-09 14:13:27 +0300

    v1

--- service/models.py
+++ service/models.py
@@ -11,7 +11,7 @@ class Service(Page):
     template = "service/service-details.html"
     service_name = models.CharField(max_length=100, blank=False, null=True)
     service_summary = models.TextField(max_length=1000, blank=False, null=True)
-    quotation_doc = models.FileField(upload_to="document", null=True)
+    quotation_doc = models.FileField(upload_to="document", null=True, )
     quotation_pdf = models.FileField(upload_to="document", null=True)
     service_details = StreamField([
 
@@ -29,6 +29,8 @@ class Service(Page):
         FieldPanel("service_summary"),
         FieldPanel("service_details"),
         FieldPanel("service_image"),
+        FieldPanel("quotation_doc"),
+        FieldPanel("quotation_pdf"),
 
     ]
 
